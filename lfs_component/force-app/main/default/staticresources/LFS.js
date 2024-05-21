5c2e7bb6ced1a73fd253dcdcdd9774babc7301ee
Ruben Halman <halmanrf@gmail.com>
2024-05-21 23:10:08 +0200

    Up Core to 3.16 (#2)
    * up core 3.16

--- lfs_component/force-app/main/default/staticresources/LFS.js
+++ lfs_component/force-app/main/default/staticresources/LFS.js
@@ -626,8 +626,8 @@
             for (const element of loopElements) {
                 let loopEnd;
                 // Check if 'noMoreValuesConnector' attribute exists
-                if (element.element['noMoreValuesConnector'] && element.element['noMoreValuesConnector'][0]) {
-                    loopEnd = element.element['noMoreValuesConnector'][0].targetReference[0];
+                if (element.element['noMoreValuesConnector'] && element.element['noMoreValuesConnector']) {
+                    loopEnd = element.element['noMoreValuesConnector'].targetReference;
                 }
                 else {
                     loopEnd = element.name;
@@ -903,7 +903,7 @@
                             let isNullOperator = false;
                             let checksIfFalse = false;
                             if (condition.leftValueReference && condition.leftValueReference.length > 0) {
-                                let valueReference = condition.leftValueReference[0];
+                                let valueReference = condition.leftValueReference;
                                 for (let ref of resultReferences) {
                                     referenceFound = ref.includes(valueReference);
                                     if (referenceFound) {
@@ -912,11 +912,11 @@
                                 }
                             }
                             if (condition.operator && condition.operator.length > 0) {
-                                let operator = condition.operator[0];
+                                let operator = condition.operator;
                                 isNullOperator = (operator === 'IsNull');
                             }
-                            if (condition.rightValue && condition.rightValue.length > 0 && condition.rightValue[0].booleanValue && condition.rightValue[0].booleanValue.length > 0) {
-                                let rightValue = condition.rightValue[0].booleanValue[0];
+                            if (condition.rightValue && condition.rightValue.length > 0 && condition.rightValue.booleanValue && condition.rightValue.booleanValue.length > 0) {
+                                let rightValue = condition.rightValue.booleanValue;
                                 checksIfFalse = (rightValue.toLowerCase() === 'false');
                             }
                             if (referenceFound && isNullOperator && checksIfFalse) {
